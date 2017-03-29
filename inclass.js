7817a579b82d01d539471dc3ced30200f3dce477
bbongutu007 <bmb117@pitt.edu>
2017-03-28 20:24:35 -0400

    CS 0134 Ben Bongutu In Class Assignment Two
    In class Assignment 2

---
+++ inclass.js
@@ -0,0 +1,16 @@
+
+  $(document).ready(function(){
+    $("h3").click(function(){
+      $(this).hide();
+    });
+  });
+  $(document).ready(function(){
+    $("h4").mouseenter(function(){
+      alert("Hi, how's your day going?");
+    });
+});
+  $(document).ready(function(){
+    $("#ball").click(function(){
+    alert("Goodbye, have a great day!");
+  });
+});
